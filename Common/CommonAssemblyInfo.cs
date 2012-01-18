21c57343f535c68fe197332275279695b02c4cc2
Rui <github@rui.fr>
2012-01-18 22:08:55 +0100

    updated assembly info

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -2,20 +2,16 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("ArtOfNet.ConfArt")]
 [assembly: AssemblyCompany("ArtOfNet")]
-[assembly: AssemblyProduct("ArtOfNet.FluentConfiguration")]
-[assembly: AssemblyCopyright("Copyright © ArtOfNet 2011")]
+[assembly: AssemblyDescription("Fluent configuration library based on conversion between flat files and dynamics")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ArtOfNet.ConfArt")]
+[assembly: AssemblyCopyright("Copyright © ArtOfNet 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-
-[assembly: Guid("813a0799-d4cc-4f4a-b060-f5e556f27679")]
-
-[assembly: AssemblyVersion("1.1.2118.0")]
-[assembly: AssemblyFileVersion("1.1.2118.0")]
+[assembly: AssemblyVersion("1.2.2118.0")]
+[assembly: AssemblyFileVersion("1.2.2118.0")]
